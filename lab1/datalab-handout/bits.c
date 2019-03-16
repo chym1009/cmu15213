ab0ae4e39e90dd171058897d8ea88adbaba4e2e9
chym1009 <shijixu1991@gmail.com>
2019-03-16 11:33:53 +0800

    20190316

--- lab1/datalab-handout/bits.c
+++ lab1/datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * <chym1009>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
