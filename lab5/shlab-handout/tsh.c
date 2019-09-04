9c4123ae4efeae0ff0a064eb7bd56d4cbc3caa3d
chym1009 <shijixu1991@gmail.com>
2019-09-04 16:01:51 +0800

    2019-09-03 tsh

--- lab5/shlab-handout/tsh.c
+++ lab5/shlab-handout/tsh.c
@@ -103,14 +103,14 @@ int main(int argc, char **argv)
         switch (c) {
         case 'h':             /* print help message */
             usage();
-	    break;
+	        break;
         case 'v':             /* emit additional diagnostic info */
             verbose = 1;
-	    break;
+	        break;
         case 'p':             /* don't print a prompt */
             emit_prompt = 0;  /* handy for automatic testing */
-	    break;
-	default:
+	        break;
+	    default:
             usage();
 	}
     }
@@ -165,6 +165,32 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];   /* Argument list execve() */
+    char buf[MAXLINE];     /* Holds modified command line */
+    int bg;                /* Should the job run in bg or fg? */ 
+    pid_t pid;             /* Process id */
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL)
+        return;
+
+    if (!builtin_cmd(argv)) {
+        if ((pid = fork()) == 0) {
+            if (execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+
+        if (!bg) {
+            int status;
+            if (waitpid(pid, &status, 0) < 0)
+                unix_error("waitfg: waitpid error");
+        }
+        else 
+            printf("%d %s", pid, cmdline);
+    }
     return;
 }
 
@@ -231,6 +257,10 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if (!strcmp(argv[0], "quit"))
+        exit(0);
+    if (!strcmp(argv[0], "&"))
+        return 1;
     return 0;     /* not a builtin command */
 }
 
